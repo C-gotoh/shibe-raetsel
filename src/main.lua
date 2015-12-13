1b6c7dac1e6a648589d22ba9f7fbc5f5ffc0227b
C-gotoh <shigotoh1@gmail.com>
2015-12-13 17:37:15 +0100

    puzzle working regardless of dimension

--- src/main.lua
+++ src/main.lua
@@ -1,11 +1,12 @@
 dimensions = {x=4,y=4}
+blankvalue = 0
 
 function updatePuzzle(clickedNumber)
 	zeroindex = {}
 	clickedindex = {}
 	for k,v in pairs(puzzle) do
 		for s,w in pairs(puzzle[k]) do
-			if w == 0 then
+			if w == blankvalue then
 				zeroindex.x = s
 				zeroindex.y = k
 			elseif w == clickedNumber then
@@ -21,23 +22,38 @@ function updatePuzzle(clickedNumber)
 	if clickedindex.x == zeroindex.x then
 		local diff = math.abs(clickedindex.y - zeroindex.y)
 		if diff <= 1 then
-				puzzle[clickedindex.x][clickedindex.y] = 0
-				puzzle[zeroindex.x][zeroindex.y] = clickedNumber
+				puzzle[clickedindex.y][clickedindex.x] = blankvalue
+				puzzle[zeroindex.y][zeroindex.x] = clickedNumber
 		end
 	end
 	if clickedindex.y == zeroindex.y then
 		local diff = math.abs(clickedindex.x - zeroindex.x)
 		if diff <= 1 then
-				puzzle[clickedindex.x][clickedindex.y] = 0
+				puzzle[clickedindex.x][clickedindex.y] = blankvalue
 				puzzle[zeroindex.x][zeroindex.y] = clickedNumber
 		end
 	end
 end
 
+function isEndCondition()
+	isEnd = true
+	for i=1,dimensions.y do
+		for j=1,dimensions.x do
+			if i==dimensions.y and j==dimensions.x then
+				break
+			elseif not puzzle[i][j] == dimensions.x*(i-1)+j then
+				isEnd = false
+				return isEnd
+			end
+		end
+	end
+	return isEnd
+end
+
 function printPuzzle()
-		for i=1,dimensions.x do
-		for k=1,dimensions.y do
-			print(puzzle[i][k])
+	for i=1,dimensions.y do
+		for j=1,dimensions.x do
+			print(puzzle[i][j])
 		end
 		print("\n")
 	end
@@ -47,14 +63,20 @@ end
 function love.load(arg)
 	defaultimg = love.graphics.newImage("data/img/img.png")
 	maxdimension = math.min(love.graphics.getWidth(), love.graphics.getHeight())
+	--init puzzle here
 	puzzle = {}
-	for i=1, dimensions.x do
-		puzzle[i] = {1+4*(i-1),2+4*(i-1),3+4*(i-1),4+4*(i-1)}
+	for i=1, dimensions.y do
+		puzzle[i] = {}
+		for j=1, dimensions.x do
+			table.insert(puzzle[i],j+dimensions.x*(i-1))
+		end
 	end
-	puzzle[4][4] = 0
-
+	puzzle[dimensions.y][dimensions.x] = blankvalue
+	--init puzzle done
 	printPuzzle()
-	updatePuzzle(4)
+	print(isEndCondition())
+	updatePuzzle(12)
+	print(isEndCondition())
 end
 
 function love.update(dt)
@@ -68,10 +90,10 @@ function love.draw(dt)
 	--print((windowheight-maxdimension)/2)
 	--love.graphics.draw(defaultimg, (windowwidth-maxdimension)/2, (windowheight-maxdimension)/2, 0, maxdimension/defaultimg:getWidth(), maxdimension/defaultimg:getHeight())
 	offset = {x=(love.graphics.getWidth()-maxdimension)/2, y=(love.graphics.getHeight()-maxdimension)/2}
-	for i=1, dimensions.x do
-		for j=1, dimensions.y do
-			print(puzzle[i][j])
-			love.graphics.print(tostring(puzzle[i][j]),offset.x+j*(maxdimension/(dimensions.x+1)),offset.y+i*(maxdimension/(dimensions.y+1)))
+	for i=1, dimensions.y do
+		for j=1, dimensions.x do
+			--print(puzzle[i][j])
+			love.graphics.print(tostring(puzzle[i][j]),offset.x+j*(maxdimension/(dimensions.x+1)),offset.y+i*(maxdimension/(dimensions.y+1)),0,2,2)
 		end
 	end
 end
